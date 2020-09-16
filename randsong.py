b6883ee4d47f7f02538b39f4543a819b7d3a5977
BuildTools <unconfigured@null.spigotmc.org>
2020-09-16 01:12:26 +0100

    secondwave

--- randsong.py
+++ randsong.py
@@ -4,38 +4,36 @@ import random, os, time
 import mutagen
 from mutagen.mp3 import MP3
 import tkinter as tk
-from tkinter import filedialog, Text
+from tkinter import filedialog, Text, messagebox
 
 ##### FRONT END AND BACK END ####
 
 root = tk.Tk()
 dires = []
-paths = dires
 
 def randSong():
-	global paths
-	random_path = (random.choice(paths))
-	files = os.listdir(random_path)
-	d = random.choice(files)
-	selectedsong = (random_path + "\\" + d)
-	print("//////////////////////////////////////////")
-	print(random_path)
-	print(files)
-	print(selectedsong)
-	print("//////////////////////////////////////////")
-	os.startfile(selectedsong)
+	global dires
+	if dires == []:
+		tk.messagebox.showerror(title="Error", message="click open file and select a folder")
+	else:
+		random_path = (random.choice(dires))
+		files = os.listdir(random_path)
+		d = random.choice(files)
+		selectedsong = (random_path + "\\" + d)
+		print(random_path)
+		print(selectedsong)
+		print("//////////////////////////////////////////")
+		os.startfile(selectedsong)
 
-def looprandsong():
-	global paths
-	random_path = (random.choice(paths))
+def looprandsong():	# not current being used in program
+	global dires
+	random_path = (random.choice(dires))
 	files = os.listdir(random_path)
 	d = random.choice(files)
 	selectedsong = (random_path + "\\" + d)
 	audio_info = MP3(selectedsong).info
 	selectedsongtime = int(audio_info.length)
-	print("//////////////////////////////////////////")
 	print(random_path)
-	print(files)
 	print(str(selectedsongtime) + " seconds in duration")
 	print(selectedsong)
 	print("//////////////////////////////////////////")
@@ -51,21 +49,29 @@ def addFolder():
 	filename = filedialog.askdirectory()
 	dires.append(filename)
 	print(filename)
+
+	if len(dires) < 1:
+		dires.remove()
+
 	for dire in dires:
 		label = tk.Label(frame, text=dire, bg="gray").pack()
 
+def clearFolders():
+	global dires
+	if dires == []:
+		tk.messagebox.showerror(title="Error", message="folder list is already empty")
+	dires = []
+	for widget in frame.winfo_children():
+		widget.destroy()
 
-canvas = tk.Canvas(root, height=400, width=400, bg="#263D42")
-canvas.pack()
+
+canvas = tk.Canvas(root, height=400, width=400, bg="#263D42").pack()
 
 frame = tk.Frame(root, bg="white")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
-openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="white", bg="#263D42", command=addFolder)
-openFile.pack()
-
-randomSong = tk.Button(root, text="Random Song", padx=10, pady=5, fg="white", bg="#263D42", command=randSong)
-randomSong.pack()
+openFile = tk.Button(root, text="Open File",  fg="white", bg="#263D42", command=addFolder).pack()
+randomSong = tk.Button(root, text="Random Song",  fg="white", bg="#263D42", command=randSong).pack()
+randomDire = tk.Button(root, text="Clear Files",  fg="white", bg="#263D42", command=clearFolders).pack()
 
 root.mainloop()
-
